bda64eb657b42c809032c8a90beae52891ca3b8b
Guilherme Malicki <guimalicki2002@gmail.com>
2025-07-29 13:06:40 -0300

    Criação do exercício 001 e 002. Primeiro Helloword e exibição de informações de servidor do PHP.

---
+++ ex000/index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da madição</title>
+</head>
+<body>
+    <h1>
+        <?php  #Supertag PHP 
+            echo "Olá, Mundo! \u{1F30E}"; // Exibe uma mensagem na tela. Também pode ser utilizado o print
+        ?>
+    </h1>
+    <p>Vamos tentar nos livrar da maldição</p>
+</body>
+</html>
